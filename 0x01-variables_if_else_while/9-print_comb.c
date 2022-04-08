afd8c03ab1a3f64a4de18785301c535bc4ad0da9
ermimak <ermiasmekonnen28@gmail.com>
2022-04-08 10:20:34 -0700

    watch out for betty

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int c = 0;
+
+
+
+	while (c < 10)
+
+	{
+
+		putchar(48 + c);
+
+		if (c != 9)
+
+		{
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+		c++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
